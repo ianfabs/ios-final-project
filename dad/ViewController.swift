4ef2a51323d1a6748e58a681d136d6ab6f101f62
Ian Fabs <ianfabs@outlook.com>
2020-03-07 14:12:07 -0500

    Initial Commit

---
+++ dad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dad
+//
+//  Created by Fabricatore, Ian T on 3/7/20.
+//  Copyright © 2020 Fabricatore, Ian T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dadTests/dadTests.swift
@@ -0,0 +1,34 @@
+//
+//  dadTests.swift
+//  dadTests
+//
+//  Created by Fabricatore, Ian T on 3/7/20.
+//  Copyright © 2020 Fabricatore, Ian T. All rights reserved.
+//
+
+import XCTest
+@testable import dad
+
+class dadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
